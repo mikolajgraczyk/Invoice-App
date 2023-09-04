f4838e17bb3fcecb21a7581e8b2dac0f0f36ed5f
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-09-04 22:27:39 +0200

    created const with selected invoice at InvoicePage

--- src/features/invoices/InvoicePage/index.js
+++ src/features/invoices/InvoicePage/index.js
@@ -1,9 +1,18 @@
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+
+import { selectInvoices } from "../invoicesSlice";
 import { StyledInvoicePage } from "./styled";
 
 const InvoicePage = () => {
+    const { id } = useParams();
+    
+    const invoicesState = useSelector(selectInvoices);
+    const selectedInvoice = invoicesState.find(invoice => invoice.id === id);
+
     return(
         <StyledInvoicePage>
-            
+            {selectedInvoice.id}
         </StyledInvoicePage>
     );
 };
